57b0ae2b07df7a49ec7656405a409780fb5731f9
Michael Luu <luum6@uci.edu>
2023-02-19 19:32:06 -0800

    changed the test post response just for a sanity check

--- routes/test.js
+++ routes/test.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.post('/', function(req, res) {
-  res.send('test');
+  res.send('response received successfully  ');
 });
 
 module.exports = router;
