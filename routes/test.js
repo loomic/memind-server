e76b898a8b771362afc03b92b87aa6d6700cca1a
Michael Luu <luum6@uci.edu>
2023-02-19 19:35:58 -0800

    added test response with json

--- routes/test.js
+++ routes/test.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.post('/', function(req, res) {
-  res.send('response received successfully  ');
+  res.send('response received successfully');
+});
+
+router.post('/json', function(req, res) {
+    res.send({message: 'response received successfully'});
 });
 
 module.exports = router;
